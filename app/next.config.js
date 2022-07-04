682862a4139ea8ad416f93a60941eff4c07de360
Adriel Telito <adrieltelito@gmail.com>
2022-07-04 19:00:30 -0300

    fixing localhost call on process.env

--- app/next.config.js
+++ app/next.config.js
@@ -6,10 +6,9 @@ const nextConfig = {
   },
   //pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   env:{
-    SKIP_PREFLIGHT_CHECK:true,
-    APP_HOST:'localhost',
-    APP_PORT:3000,
-    SKIP_PREFLIGHT_CHECK:true,
+    SKIP_PREFLIGHT_CHECK:process.env.SKIP_PREFLIGHT_CHECK,
+    APP_HOST:process.env.APP_HOST,
+    APP_PORT:process.env.APP_PORT,
     REACT_APP_URL:process.env.REACT_APP_URL,
     REACT_APP_SEGURANCA_API:process.envREACT_APP_SEGURANCA_API,
     REACT_APP_GERAL_API:process.env.REACT_APP_GERAL_API,
