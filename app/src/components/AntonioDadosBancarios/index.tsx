3983d246c5bb40ff9d9a28d55450a3d222273d94
@LucasMtss <lmatos@f3capital.com.br>
2022-10-06 10:17:33 -0300

    fix: fix dropdowns in menu

--- app/src/components/AntonioDadosBancarios/index.tsx
+++ app/src/components/AntonioDadosBancarios/index.tsx
@@ -5,7 +5,7 @@ import {
   AntonioPersonagem,
   AntonioIconClose,
 } from './style';
-import AntonioSuperior  from '../../assets/antonio-superior-dados-bancarios.svg';
+import AntonioSuperior from '../../assets/antonio-superior-dados-bancarios.svg';
 import { IoMdCloseCircleOutline } from 'react-icons/io';
 import { AZUL } from '../../styles/variaveis';
 
