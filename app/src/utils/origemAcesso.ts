c128dbe94bc4fbee69eea6ab1126342e6ad6dd4d
@LucasMtss <lmatos@f3capital.com.br>
2022-11-29 22:40:28 -0300

    feat: add origin access utils

---
+++ app/src/utils/origemAcesso.ts
@@ -0,0 +1,19 @@
+import { setLocalStorage, getLocalStorage } from './../hooks/localStorage';
+
+export function salvarOrigemAcesso(link?: string) {
+  setLocalStorage('origem_acesso', link || window.location.href);
+}
+
+export function obterOrigemAcesso() {
+  return getLocalStorage('origem_acesso');
+}
+
+export function gerarUrl(url: string) {
+  const urlAtual = window.location.href;
+  const posicaoBarra = urlAtual.indexOf('/', 9);
+  return `${urlAtual.slice(0, posicaoBarra)}/${url}`;
+}
+
+export function resetOrigemAcesso() {
+  localStorage.removeItem('@freelas_town:origem_acesso');
+}
