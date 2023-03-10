88967f955a1640fa8554e56a5e46a1574d5d5616
Sr.Lopes <lucasleme90@gmail.com>
2023-03-10 00:47:20 -0300

    fix tf backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "812671651954-statefile"
+    bucket = "184313004318-terraform-state-repo"
     workspace_key_prefix = "next"
     key    = "task.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
