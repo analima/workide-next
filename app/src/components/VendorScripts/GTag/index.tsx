cb70f3e3cecfcdfb788e9ba4bfd780d600b6d81e
Sr.Lopes <lucasleme90@gmail.com>
2023-03-16 22:07:52 -0300

    fix gtag

--- app/src/components/VendorScripts/GTag/index.tsx
+++ app/src/components/VendorScripts/GTag/index.tsx
@@ -3,13 +3,13 @@ import Script from 'next/script';
 export function GTag() {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-244663367-1" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-6BGQ8L6H1J" />
       <Script id="gtag">
         {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-244663367-1');`}
+  gtag('config', 'G-6BGQ8L6H1J');`}
       </Script>
     </>
   );
